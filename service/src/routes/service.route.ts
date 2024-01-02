2f01caefd48cc9a7793c5929b666d6dd1729cd7f
Luís Pinto <luis.pinto@commercetools.com>
2024-01-02 18:21:26 +0000

    chore: update service endpoint response

--- service/src/routes/service.route.ts
+++ service/src/routes/service.route.ts
@@ -5,7 +5,7 @@ const serviceRouter = Router();
 
 serviceRouter.get('/', (req, res) => {
   res.status(200);
-  res.send({ status: 'ok', version: 1 });
+  res.send({ status: 'ok', version: 2 });
 });
 
 export default serviceRouter;
